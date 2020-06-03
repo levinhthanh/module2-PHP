84060623454d1f32b1533459ddd752a22609049b
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-06-03 13:46:11 +0700

    add server time

---
+++ tongQuan/serverTime/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Thời gian hiện tại</title>
+     </head>
+     <body>
+         <h1> Bây giờ là: <?php echo date('Y-M-d-D h:m:s'); ?> </h1>
+     </body>
+</html>
