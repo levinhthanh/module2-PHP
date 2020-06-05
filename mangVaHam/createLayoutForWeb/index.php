60795443c1d71abc86ecfd4666c870aa7b6240fc
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-06-05 08:51:47 +0700

    add create layout

---
+++ mangVaHam/createLayoutForWeb/index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <link rel="stylesheet" type="text/css"href="style.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="container">
+            <header><?php  require "header.php";  ?></header>
+            <nav><?php require "nav.php";  ?></nav>
+            <article> <?php require "content.php"; ?></article>
+            <footer><?php require "footer.php";?></footer>
+        </div>
+    </body>
+</html>
