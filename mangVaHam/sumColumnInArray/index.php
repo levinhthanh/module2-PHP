786311c151db4ca1b459da910ee539a2f3fd1547
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-06-07 12:31:12 +0700

    modify index

--- mangVaHam/sumColumnInArray/index.php
+++ mangVaHam/sumColumnInArray/index.php
@@ -21,20 +21,19 @@
 
             <?php
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                // if (isset($_POST['len'])) {
                 $len = $_POST['len'];
                 $wid = $_POST['wid'];
                 $col = $_POST['col'];
                 $arr = [];
                 $sum = 0;
                 for ($i = 0; $i < $len; $i++) {
-                    for ($j = 0; $j < $len; $j++) {
+                    for ($j = 0; $j < $wid; $j++) {
                         $arr[$i][$j] = $_POST["arr$i-$j"];
                     }
                 }
                 if ($col <= ($wid - 1)) {
                     for ($i = 0; $i < $len; $i++) {
-                        for ($j = 0; $j < $len; $j++) {
+                        for ($j = 0; $j < $wid; $j++) {
                             if ($j == $col) {
                                 $sum += $arr[$i][$j];
                             }
@@ -47,7 +46,6 @@
                     echo "<br> Cột bạn vừa nhập không tồn tại. ";
                 }
             }
-            // }
             ?>
         </fieldset>
     </form>
@@ -55,36 +53,36 @@
         <table id="table">
             <!-- put in here -->
         </table>
-        <span id="span" style="display: none">Nhập cột bạn muốn cộng:</span>
+        <span id="span" style="display: none">Nhập cột bạn muốn cộng [0 , ... , n]:</span>
         <input id="column" type="text" name="col" style="display: none">
         <input id="submit" type="submit" style="display: none">
         <input id="l" type="text" name="len" style="display: none">
         <input id="w" type="text" name="wid" style="display: none">
     </form>
     <script>
-        let len
-        let wid
-
+        let len;
+        let wid;
         function inputData() {
             len = parseInt(document.getElementById('length').value);
             wid = parseInt(document.getElementById('width').value);
-
-            document.getElementById('l').value = len;
-            document.getElementById('w').value = wid;
-            document.getElementById('length').placeholder = toString(len);
-            document.getElementById('width').placeholder = toString(wid);
-            let draw = '';
-            for (let i = 0; i < len; i++) {
-                draw += '<tr>';
-                for (let j = 0; j < wid; j++) {
-                    draw += '<td>Array[' + i + ',' + j + '] = <input type="text" name="arr' + i + '-' + j + '"></td>';
+            if (!isNaN(len) && !isNaN(wid)) {
+                document.getElementById('l').value = len;
+                document.getElementById('w').value = wid;
+                document.getElementById('length').placeholder = toString(len);
+                document.getElementById('width').placeholder = toString(wid);
+                let draw = '';
+                for (let i = 0; i < len; i++) {
+                    draw += '<tr>';
+                    for (let j = 0; j < wid; j++) {
+                        draw += '<td>Array[' + i + ',' + j + '] = <input type="text" name="arr' + i + '-' + j + '"></td>';
+                    }
+                    draw += '</tr>';
                 }
-                draw += '</tr>';
+                document.getElementById('table').innerHTML = draw;
+                document.getElementById('submit').style = "display: block";
+                document.getElementById('column').style = "display: block";
+                document.getElementById('span').style = "display: block";
             }
-            document.getElementById('table').innerHTML = draw;
-            document.getElementById('submit').style = "display: block";
-            document.getElementById('column').style = "display: block";
-            document.getElementById('span').style = "display: block";
         }
     </script>
 </body>
