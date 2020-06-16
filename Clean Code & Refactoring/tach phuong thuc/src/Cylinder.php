df9602a68323c1915b5414ec540669e13b7c0132
LE VINH THANH <levinhthanh.dt1@gmail.com>
2020-06-16 10:36:13 +0700

    add tach phuongthuc

---
+++ Clean Code & Refactoring/tach phuong thuc/src/Cylinder.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: phanluan
+ * Date: 26/10/2018
+ * Time: 22:50
+ */
+// class Cylinder
+// {
+//     public function getVolume($radius, $height)
+//     {
+//         $baseArea = pi() * $radius * $radius;
+//         $perimeter = 2 * pi() * $radius;
+//         $volume = $perimeter * $height + 2 * $baseArea;
+//         return $volume;
+//     }
+// }
+
+class Cylinder
+{
+    public function getVolume($radius, $height)
+    {
+        $baseArea = $this->getPerimeter($radius);
+        $perimeter = $this->getBaseArea($radius);
+        $volume = $perimeter * $height + 2 * $baseArea;
+        return $volume;
+    }
+
+    /**
+     * @param $radius
+     * @return float|int
+     */
+    public function getPerimeter($radius)
+    {
+        $baseArea = pi() * $radius * $radius;
+        return $baseArea;
+    }
+
+    /**
+     * @param $radius
+     * @return float|int
+     */
+    public function getBaseArea($radius)
+    {
+        $perimeter = 2 * pi() * $radius;
+        return $perimeter;
+    }
+}
